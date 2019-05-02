ef1108a6b09761097ae7d63f963bbd4409e054f9
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-05-02 15:33:12 +0200

    a

---
+++ build.sh
@@ -0,0 +1,1 @@
+sass ./frontend/scss/style.scss ./frontend/css/style.css
--- frontend/js/main-application.js
+++ frontend/js/main-application.js
@@ -1,9 +1,18 @@
+var standard_title = "DesignerMadsen";
+
+function setTitle(title)
+{
+  document.title = title;
+}
+
+setTitle( standard_title );
+
 var application = new Vue(
   {
     el:'#application',
     data:
     {
-      
+
     },
     watch:
     {
